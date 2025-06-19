6a1db5e790a625f41a1ffe2827541a3cc0dee6c1
Depayss <Christophermaomao@gmail.com>
2025-06-18 21:46:19 -0400

    manual cards set up

---
+++ src/components/Cards.tsx
@@ -0,0 +1,31 @@
+
+export default function Cards(props:any){
+    return(
+        <section className="cards-container">
+            <article className="card">
+                <header className="card-header">
+                    <h1>
+                        Arabesque No.1 in E Major
+                    </h1>
+                    <button>Details</button>
+                </header>
+                <div className="detail">
+                    <h3>Debussy</h3>
+                    <h3>O O O O O</h3>
+                </div>
+            </article>
+            <article className="card">
+                <header className="card-header">
+                    <h1>
+                        Arabesque No.1 in E Major
+                    </h1>
+                    <button>Details</button>
+                </header>
+                <div className="detail">
+                    <h3>Debussy</h3>
+                    <h3>O O O O O</h3>
+                </div>
+            </article>
+        </section>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header className="web-header">
+      <div className="header-container">
+
+          <h1>Music Diary</h1>
+
+        <section className="user">
+          <h2>SHEETS</h2>
+          <h2>LISTS</h2>
+          <button>+LOG</button>
+        </section>
+      </div>
+    </header>
+  );
+}
