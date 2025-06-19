36e7888fb43d6340040eba43dd98a36ae0a3cb2e
Depayss <Christophermaomao@gmail.com>
2025-06-18 22:12:42 -0400

    fixed cards visuals

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -26,6 +26,18 @@ export default function Cards(props:any){
                     <h3>O O O O O</h3>
                 </div>
             </article>
+             <article className="card">
+                <header className="card-header">
+                    <h1>
+                        Arabesque No.1 in E Major
+                    </h1>
+                    <button>Details</button>
+                </header>
+                <div className="detail">
+                    <h3>Debussy</h3>
+                    <h3>O O O O O</h3>
+                </div>
+            </article>
         </section>
     )
 }
